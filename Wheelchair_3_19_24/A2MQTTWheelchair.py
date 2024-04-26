fe20657d0e977e3aa74234164af87ed2ef21bd7f
nicolaustvalle <165834541+nicolaustvalle@users.noreply.github.com>
2024-04-26 10:30:58 -0400

    Update A2MQTTWheelchair.py

--- Wheelchair_3_19_24/A2MQTTWheelchair.py
+++ Wheelchair_3_19_24/A2MQTTWheelchair.py
@@ -10,8 +10,8 @@ GPIO.setup(38, GPIO.OUT)
 
 # Creating motor object using GPIO pins 16, 17, and 18
 # (using SN754410 quadruple half-H driver chip)
-mymotor1 = Motor(enable1=16, pwm1=17, pwm2=18)
-mymotor2 = Motor(enable1=6, pwm1=14, pwm2=15)
+mymotor1 = Motor(enable1=20, enable2 = 21, pwm1=19, pwm2=26)
+mymotor2 = Motor(enable1=11, enable2 = 9, pwm1=18, pwm2=17)
 MQTT_TOPIC = [("vmi\Direction",2),("vmi\Speed",1),("vmi\Brake",0)]
 
 dirr=""
